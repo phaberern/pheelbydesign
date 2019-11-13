3edd39ca89c812ab33dc30408cc50f43e84eda11
Phil <phillip.haberern5@gmail.com>
2019-11-12 19:38:26 -0500

    did stuff can't remember'

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as serviceWorker from './util/serviceWorker';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
 
-import Container from '@material-ui/core/Container';
+import Container from "@material-ui/core/Container"
 
-ReactDOM.render(
-<Container  fixed>
+ReactDOM.render(<Container>
     <h1>Pheel By Design</h1>
-</Container>
-, document.getElementById('root'));
+</Container>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
